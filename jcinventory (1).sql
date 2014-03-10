7d6760e73282e60f9e1759ffa54109860d606040
Rongie Andrade <rrongie@gmail.com>
2014-03-10 20:57:02 +0800

    db

---
+++ jcinventory (1).sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 10, 2014 at 01:48 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `jcinventory`
+--
+CREATE DATABASE IF NOT EXISTS `jcinventory` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `jcinventory`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE IF NOT EXISTS `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `name`, `last_update`) VALUES
+(1, 'IT department', '2014-03-05 10:43:32'),
+(3, 'Accounting', '2014-03-10 02:40:52'),
+(4, 'Leasing', '2014-03-10 02:41:25'),
+(5, 'New Department', '2014-03-10 05:05:37'),
+(6, 'Again ', '2014-03-10 05:10:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplier_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `item_brand` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `item_type` enum('Fixed','Consumable','','') NOT NULL,
+  `item_unit` varchar(255) NOT NULL,
+  `item_qty` int(11) NOT NULL,
+  `item_price` float(10,2) NOT NULL,
+  `item_serial` varchar(255) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `supplier_id`, `department_id`, `item_brand`, `item_name`, `item_type`, `item_unit`, `item_qty`, `item_price`, `item_serial`, `date_add`) VALUES
+(1, 2, 3, 'eqweqweqw', 'eqeqeqqew', '', '', 23, 3.00, '', '2014-03-09 13:53:15'),
+(2, 2, 3, 'asdada', 'dadada', '', '', 3, 3.00, '', '2014-03-09 13:53:25'),
+(3, 2, 3, 'asdasa', 'asdad', '', '', 2323, 3.00, '', '2014-03-09 07:03:29'),
+(4, 0, 0, 'brand', 'mineral water', '', 'container', 5, 200.00, '2345', '2014-03-10 02:42:33'),
+(5, 1, 0, 'brand', 'mineral water', '', 'container', 4, 23.00, '9999', '2014-03-10 02:46:56'),
+(6, 9, 4, 'brand', 'mineral water', '', 'container', 4, 43.00, '345', '2014-03-10 02:57:27'),
+(7, 1, 1, 'test', 'chair', 'Fixed', 'chair', 7, 6.00, '879978', '2014-03-10 03:02:29'),
+(8, 9, 4, 'brand', 'epson ink cartridge 141 black', 'Consumable', 'pcs', 7, 6.00, '343242', '2014-03-10 03:04:27'),
+(9, 9, 1, 'last', 'last', 'Consumable', 'last', 3, 23.00, '654', '2014-03-10 03:05:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personnel`
+--
+
+CREATE TABLE IF NOT EXISTS `personnel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `type` enum('staff','admin','user','','','') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `personnel`
+--
+
+INSERT INTO `personnel` (`id`, `username`, `password`, `fname`, `lname`, `type`) VALUES
+(1, 'rrongie', '5f4dcc3b5aa765d61d8327deb882cf99', 'wrong', 'G', 'admin'),
+(2, 'staff', '5f4dcc3b5aa765d61d8327deb882cf99', 'katz', 'poy', 'staff'),
+(8, 'katz123', '5f4dcc3b5aa765d61d8327deb882cf99', 'katz', 'katz', 'staff'),
+(9, 'test123', '5f4dcc3b5aa765d61d8327deb882cf99', 'test', 'test', 'staff');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE IF NOT EXISTS `supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplier_fname` varchar(255) NOT NULL,
+  `supplier_lname` varchar(255) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `supplier_fname`, `supplier_lname`, `address`, `mobile`) VALUES
+(1, 'katz', 'vinz', 'talamban Cebu City', '0999999999'),
+(2, 'vinz', 'Oblad', 'Lahug Cebu City', '09888888888'),
+(9, 'Rongie', 'Andrade', 'Bacyan Cebu City', '09353433738'),
+(10, 'Mariz', 'Mariz', 'Talamban Cebu City', '0999999999'),
+(11, 'Bry', 'secret', 'Cebu City', '09297982124');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
