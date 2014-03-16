fcf7908456f9c48ea9c94a886eebbf3e40df8853
Bryan Bojorque <bojorquebryan@gmail.com>
2014-03-16 17:14:28 +0100

    purchase order

--- application/libraries/bryan.php
+++ application/libraries/bryan.php
@@ -7,12 +7,10 @@ class bryan extends FPDF
 
 
 function Footer(){
-	$this->Ln(60);
+	$this->Ln(30);
 	$this->SetFont('Arial','',15);
 	$this->Cell(170,10,'Remarks','',0,'L');
 	$this->Ln(20);	
-	$this->Line(10, 170, 200, 170);
-	$this->Ln(60);
 	$this->SetFont('Arial','',12);
 	$this->Cell(170,10,'Issued By','',0,'L');
 	$this->Cell(170,10,'Date','',0,'L');
