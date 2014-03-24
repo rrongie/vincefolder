b5ed3428a89065f6d4773f5dc03f3a6ea16aac64
Bryan Bojorque <bojorquebryan@gmail.com>
2014-03-24 21:48:10 +1030

    nv

--- application/views/admin/admin_nav.php
+++ application/views/admin/admin_nav.php
@@ -33,12 +33,21 @@
 
            </li>
 
+
+<li class="dropdown" style="font-size:15px; margin-top:5px;">
+
+              <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">LISTS</a>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+                <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/purchase_list'); ?>">PURCHASES LIST</a></li >
+                <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/borrowers'); ?>">BORROWERS LIST</a></li>
+              </ul>
+
+           </li>
+
 <li class="dropdown" style="font-size:15px; margin-top:5px;">
 
               <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">REPORTS</a>
               <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
-                <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/purchase_list'); ?>">PURCHASES LIST</a></li >
-                <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/borrowers'); ?>">BORROWERS LIST</a></li>
                 <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/receiving'); ?>">RECEIVING REPORT</a></li>
                 <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/reconcile'); ?>">RECONCILE REPORT</a></li>
                   <li role="presentation"><a style="font-size:15px; margin-top:5px;" role="menuitem" tabindex="-1" href="<?php echo site_url('admin/reconcile'); ?>">CONSUMABLE REPORT</a></li>
